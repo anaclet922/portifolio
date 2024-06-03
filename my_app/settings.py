04767db973b86dc22050952e0cd36e0c9bcea4ed
Anaclet Ahishakiye <a.anaclet920@gmail.com>
2024-06-03 11:52:51 +0200

    Update 03062024

--- my_app/settings.py
+++ my_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a3jm5pgp)gktj9vrpugbao(^xhspbs!b+6l1)7t2i4k*w)^05&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['anaclet.online', 'www.anaclet.online']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
