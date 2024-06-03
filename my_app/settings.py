f5eb29966a78ceab03e41f4c56c5cd29942b952c
Anaclet Ahishakiye <a.anaclet920@gmail.com>
2024-06-03 15:08:05 +0200

    Update 03062024

--- my_app/settings.py
+++ my_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a3jm5pgp)gktj9vrpugbao(^xhspbs!b+6l1)7t2i4k*w)^05&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
@@ -119,6 +119,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
